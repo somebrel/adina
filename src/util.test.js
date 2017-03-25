e0b056e3015f1b6f2b027bffe0bd2035145bfda1
Brahian E. Soto Mercedes <brahian@protonmail.com>
2017-03-25 02:54:18 +0100

    Refactors util.test to use ES6 imports.

--- src/util.test.js
+++ src/util.test.js
@@ -1,21 +1,14 @@
-const util = require("./util");
+import { arraysIdentic, removeFromArray, pickRandomFromArray } from "./util";
 
-const arraysIdentic = util.arraysIdentic;
 test("Test if two arrays are identic", () => {
   expect(arraysIdentic([1, 2, 3, 4], [1, 2, 3, 4])).toBe(true);
 });
 
-const removeFromArray = util.removeFromArray;
-test("Removes one element from an array in a 'pure' way", () => {
-  expect(util.arraysIdentic(removeFromArray([1, 2, 3, 3], 3), [1, 2, 3])).toBe(
-    true
-  );
-  expect(util.arraysIdentic(removeFromArray([1, 2, 3, 4], 3), [1, 2, 4])).toBe(
-    true
-  );
+test("Removes one element from an array in a 'functional' way", () => {
+  expect(arraysIdentic(removeFromArray([1, 2, 3, 3], 3), [1, 2, 3])).toBe(true);
+  expect(arraysIdentic(removeFromArray([1, 2, 3, 4], 3), [1, 2, 4])).toBe(true);
 });
 
-const pickRandomFromArray = util.pickRandomFromArray;
 test("Picks 'n' random elements from an array.", () => {
   expect(pickRandomFromArray([], 3).length).toBe(0);
   expect(pickRandomFromArray([1], 3).length).toBe(0);
