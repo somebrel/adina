c834948ceb5f100cd58b73a50f24a8fb846fa089
Brahian E. Soto Mercedes <brahian@protonmail.com>
2017-03-25 12:31:46 +0100

    Adds more unit test for removeFromArray().

--- src/util.test.js
+++ src/util.test.js
@@ -12,6 +12,15 @@ test("Test if two arrays are identic", () => {
 });
 
 test("Removes one element from an array in a 'functional' way", () => {
+  expect(removeFromArray()).toBe(null);
+  expect(removeFromArray([1, 2, 3, 4])).toBe(null);
+  expect(removeFromArray(2)).toBe(null);
+
+  let emptyArr = [];
+  let arr = [1, 2, 3, 4];
+  expect(removeFromArray(arr, 5)).toBe(arr);
+  expect(removeFromArray(emptyArr, 5)).toBe(emptyArr);
+
   expect(arraysIdentic(removeFromArray([1, 2, 3, 3], 3), [1, 2, 3])).toBe(true);
   expect(arraysIdentic(removeFromArray([1, 2, 3, 4], 3), [1, 2, 4])).toBe(true);
 });
