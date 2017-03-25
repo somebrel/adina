3307e414e13e95b6c656b1ed6799162c16ab1bd2
Brahian E. Soto Mercedes <brahian@protonmail.com>
2017-03-25 03:03:36 +0100

    Refactor: uses 'export' instead of 'module.exports'.

--- src/util.js
+++ src/util.js
@@ -1,55 +1,51 @@
-const util = {
-  removeFromArray(arr, element) {
-    let elIndex = arr.indexOf(element);
+export function removeFromArray(arr, element) {
+  let elIndex = arr.indexOf(element);
 
-    let arr1 = arr.slice(0, elIndex);
-    let arr2 = arr.slice(elIndex + 1, arr.length);
+  let arr1 = arr.slice(0, elIndex);
+  let arr2 = arr.slice(elIndex + 1, arr.length);
 
-    return arr1.concat(arr2);
-  },
+  return arr1.concat(arr2);
+}
 
-  arraysIdentic(arr1, arr2) {
-    if (arr1 === arr2) return true;
-    if (arr1 == null || arr2 == null) return false;
-    if (arr1.length != arr2.length) return false;
+export function arraysIdentic(arr1, arr2) {
+  if (arr1 === arr2) return true;
+  if (arr1 == null || arr2 == null) return false;
+  if (arr1.length != arr2.length) return false;
 
-    for (var i = 0; i < arr1.length; ++i) {
-      if (arr1[i] !== arr2[i]) return false;
-    }
-    return true;
-  },
-
-  pickRandomFromArray(arr = [], n = 0) {
-    if (n === 0) return [];
-    if (arr.length === 0) {
-      console.error("Error: Empty array.");
-      return [];
-    }
-    if (arr.length < n) {
-      console.error("Error: More elements requested then available.");
-      return [];
-    }
-    if (arr.length === n) {
-      console.warn("Warning: Same amout of elements requested as available.");
-      console.warn("-> Sorting elements randomly.");
-    }
-
-    const randomList = [];
-    let originalList = arr;
-
-    for (let i = 0; i < n; i++) {
-      // gets a random element from originalList
-      let randomEl = originalList[
-        Math.floor(Math.random() * originalList.length)
-      ];
-      // removes picked element from originalList
-      originalList = util.removeFromArray(originalList, randomEl);
-      // puts picked element in randomList
-      randomList.push(randomEl);
-    }
-
-    return randomList;
+  for (var i = 0; i < arr1.length; ++i) {
+    if (arr1[i] !== arr2[i]) return false;
   }
-};
+  return true;
+}
 
-module.exports = util;
+export function pickRandomFromArray(arr = [], n = 0) {
+  if (n === 0) return [];
+  if (arr.length === 0) {
+    console.error("Error: Empty array.");
+    return [];
+  }
+  if (arr.length < n) {
+    console.error("Error: More elements requested then available.");
+    return [];
+  }
+  if (arr.length === n) {
+    console.warn("Warning: Same amout of elements requested as available.");
+    console.warn("-> Sorting elements randomly.");
+  }
+
+  const randomList = [];
+  let originalList = arr;
+
+  for (let i = 0; i < n; i++) {
+    // gets a random element from originalList
+    let randomEl = originalList[
+      Math.floor(Math.random() * originalList.length)
+    ];
+    // removes picked element from originalList
+    originalList = removeFromArray(originalList, randomEl);
+    // puts picked element in randomList
+    randomList.push(randomEl);
+  }
+
+  return randomList;
+}
